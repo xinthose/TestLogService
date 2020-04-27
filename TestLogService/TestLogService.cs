29b804ac42ee03978aaebf7ae4420436dd728734
Adam Dunsmoor <adamdunsmoor@pm.me>
2020-04-26 21:54:28 -0500

    no more errors!

--- TestLogService/TestLogService.cs
+++ TestLogService/TestLogService.cs
@@ -10,35 +10,39 @@ using System.Threading.Tasks;
 using System.Timers;
 using System.Runtime.InteropServices;
 
-public enum ServiceState
-{
-    SERVICE_STOPPED = 0x00000001,
-    SERVICE_START_PENDING = 0x00000002,
-    SERVICE_STOP_PENDING = 0x00000003,
-    SERVICE_RUNNING = 0x00000004,
-    SERVICE_CONTINUE_PENDING = 0x00000005,
-    SERVICE_PAUSE_PENDING = 0x00000006,
-    SERVICE_PAUSED = 0x00000007,
-}
-
-[StructLayout(LayoutKind.Sequential)]
-public struct ServiceStatus
-{
-    public int dwServiceType;
-    public ServiceState dwCurrentState;
-    public int dwControlsAccepted;
-    public int dwWin32ExitCode;
-    public int dwServiceSpecificExitCode;
-    public int dwCheckPoint;
-    public int dwWaitHint;
-};
 
 namespace TestLogService
 {
+    public enum ServiceState
+    {
+        SERVICE_STOPPED = 0x00000001,
+        SERVICE_START_PENDING = 0x00000002,
+        SERVICE_STOP_PENDING = 0x00000003,
+        SERVICE_RUNNING = 0x00000004,
+        SERVICE_CONTINUE_PENDING = 0x00000005,
+        SERVICE_PAUSE_PENDING = 0x00000006,
+        SERVICE_PAUSED = 0x00000007,
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct ServiceStatus
+    {
+        public int dwServiceType;
+        public ServiceState dwCurrentState;
+        public int dwControlsAccepted;
+        public int dwWin32ExitCode;
+        public int dwServiceSpecificExitCode;
+        public int dwCheckPoint;
+        public int dwWaitHint;
+    };
 
     public partial class TestLogService : ServiceBase
     {
         private int eventId = 1;
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool SetServiceStatus(System.IntPtr handle, ref ServiceStatus serviceStatus);
+
 
         public void OnTimer(object sender, ElapsedEventArgs args)
         {
@@ -53,6 +57,12 @@ namespace TestLogService
 
         protected override void OnStart(string[] args)
         {
+            // Update the service state to Start Pending.
+            ServiceStatus serviceStatus = new ServiceStatus();
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_START_PENDING;
+            serviceStatus.dwWaitHint = 100000;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+
             eventLog1.WriteEntry("In OnStart.");
 
             // Set up a timer that triggers every minute.
@@ -61,15 +71,10 @@ namespace TestLogService
             timer.Elapsed += new ElapsedEventHandler(this.OnTimer);
             timer.Start();
 
-            [DllImport("advapi32.dll", SetLastError = true)]
-            private static extern bool SetServiceStatus(System.IntPtr handle, ref ServiceStatus serviceStatus);
-
-            // Update the service state to Start Pending.
-            ServiceStatus serviceStatus = new ServiceStatus();
-            serviceStatus.dwCurrentState = ServiceState.SERVICE_START_PENDING;
-            serviceStatus.dwWaitHint = 100000;
+            // Update the service state to Running.
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_RUNNING;
             SetServiceStatus(this.ServiceHandle, ref serviceStatus);
-        }
+    }
 
         protected override void OnStop()
         {
